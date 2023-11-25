18a7d0e775db44cdbe0b0037ba39b661485ff887
kevin2813 <kevin.rodast.95@gmail.com>
2023-11-25 14:28:44 -0300

    added: tailwind + flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ADS-Frontend';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
